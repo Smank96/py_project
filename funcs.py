c423fd691f74705567d53a921d6ce71cf61b1992
Dmitriy Ovchinnikov <smank96@gmail.com>
2024-04-16 19:41:15 +0700

    third commit from pycharm.

--- funcs.py
+++ funcs.py
@@ -2,6 +2,7 @@ def sum_of_numbers(a: int, b: int) -> int:
     """Складывает 2 целых числа."""
     return a + b
 
+
 def subtraction(a: int, b: int) -> int:
     """Возвращает разницу между числами a и b."""
     return a - b
---
+++ main.py
@@ -0,0 +1,3 @@
+from funcs import sum_of_numbers
+
+print(sum_of_numbers(a=10, b=16))
